333c005d6969390b4fb625caab5f05637709a3d3
deepsj1012 <deepsj1012@naver.com>
2023-09-30 23:46:37 +0900

    테스트 코드 동작 확인

--- ex1/src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
+++ ex1/src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Ex1ApplicationTests {
-
     @Test
     void contextLoads() {
+        System.out.println("contextLoads...");
     }
 
 }

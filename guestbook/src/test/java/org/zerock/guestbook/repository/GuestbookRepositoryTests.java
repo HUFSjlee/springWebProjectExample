5e1b8b8a644d5d968a9b4ffb18fa65e463129794
deepsj1012 <deepsj1012@naver.com>
2023-10-24 23:08:19 +0900

    Add query test method using QueryDSL

--- guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,10 +1,17 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -41,5 +48,26 @@ public class GuestbookRepositoryTests {
 
             guestbookRepository.save(guestbook);
         }
+    }
+
+    @Test
+    public void testQuery1() {
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; //1
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder(); //2
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword); //3
+
+        builder.and(expression); //4
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); //5
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
     }
 }

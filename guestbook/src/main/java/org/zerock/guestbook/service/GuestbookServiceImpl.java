778091863753763db2438e7d50de2d6c3944f59e
deepsj1012 <deepsj1012@naver.com>
2023-10-26 22:25:48 +0900

    fix GuestbookServiceImpl

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,13 +1,19 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements  GuestbookService {
+
+    private final GuestbookRepository repository;
+
     @Override
     public Long register(GuestbookDTO dto) {
         log.info("DTO-------------");
@@ -17,6 +23,8 @@ public class GuestbookServiceImpl implements  GuestbookService {
 
         log.info(entity);
 
-        return null;
+        repository.save(entity);
+
+        return entity.getGno();
     }
 }

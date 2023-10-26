458db07903de2f16f3907bf55cdb533b7ac5324b
deepsj1012 <deepsj1012@naver.com>
2023-10-26 22:38:44 +0900

    fix testList test method

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -1,11 +1,14 @@
 package org.zerock.guestbook.service;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.
         test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
 public class GuestbookServiceTests {
@@ -27,6 +30,12 @@ public class GuestbookServiceTests {
     @Test
     public void testList() {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
     }
 
 }

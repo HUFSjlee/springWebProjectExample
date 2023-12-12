3bfb10e83b922ec8ad505cd21d9a05de2f59808f
deepsj1012 <deepsj1012@naver.com>
2023-12-12 21:56:29 +0900

    Add Board Entity attribute

--- board/src/main/java/org/zerock/board/entity/Board.java
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -1,4 +1,25 @@
 package org.zerock.board.entity;
 
-public class Board {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
 }

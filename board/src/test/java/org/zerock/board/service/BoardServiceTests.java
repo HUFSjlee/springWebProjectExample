7acba722267828431c3ad198754b051e7cb2dd34
deepsj1012 <deepsj1012@naver.com>
2023-12-20 23:26:05 +0900

    Add BoardServiceTests

--- board/src/test/java/org/zerock/board/service/BoardServiceTests.java
+++ board/src/test/java/org/zerock/board/service/BoardServiceTests.java
@@ -3,7 +3,6 @@ package org.zerock.board.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.zerock.board.dto.BoardDTO;
 
 @SpringBootTest

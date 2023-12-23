18bb6ce0f08efd06c15b22a15ee823e83abc1a29
deepsj1012 <deepsj1012@naver.com>
2023-12-23 22:58:41 +0900

    Fix PageRequestDTO

--- board/src/main/java/org/zerock/board/service/BoardServiceImpl.java
+++ board/src/main/java/org/zerock/board/service/BoardServiceImpl.java
@@ -2,6 +2,8 @@ package org.zerock.board.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.entity.Board;
@@ -31,6 +33,10 @@ public class BoardServiceImpl implements BoardService {
     public PageRequestDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
         log.info(pageRequestDTO);
 
-        Function<Object[], BoardDTO> fn = (en -> entityToDTO(Board)en[0,(Member)en[1],(Long)en[2]]);
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board)en[0],(Member)en[1],(Long)en[2]));
+
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        return new PageRequest<>(result, fn);
     }
 }

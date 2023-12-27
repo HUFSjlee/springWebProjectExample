95ebef3889c87cbea15626831ca5a691a0d3870f
deepsj1012 <deepsj1012@naver.com>
2023-12-27 23:55:12 +0900

    Fix

--- board/src/test/java/org/zerock/board/service/BoardServiceTests.java
+++ board/src/test/java/org/zerock/board/service/BoardServiceTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.dto.BoardDTO;
-import org.zerock.board.dto.PageRequestDTO;
 
 @SpringBootTest
 public class BoardServiceTests {
